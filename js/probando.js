2be35065ea4de983f94d18d7c0613462ccdb6757
Gabriel <ludwing.paz@gmail.com>
2021-12-06 01:00:07 -0600

    Updates

--- js/probando.js
+++ js/probando.js
@@ -1,7 +1,8 @@
 const boton = document.getElementById('probando')
-import { v4 as uuidv4 } from 'uuid';
+
 
 
 boton.addEventListener('click',()=>{
-    alert(uuidv4())
+    alert('ahora si xd')
+    document.getElementById("entrada").textContent='simona la mona'
 })
