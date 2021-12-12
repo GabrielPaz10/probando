258a6741d53700bb011c219750378ca30b1322d2
Gabriel <ludwing.paz@gmail.com>
2021-12-11 21:33:16 -0600

    Updates

--- js/probando.js
+++ js/probando.js
@@ -1,10 +1,7 @@
 const ejecutar = document.getElementById('ejecutar')
 const consola = document.getElementById('consola')
 let contenido =""
-//let cm1 = new CodeMirror();
 
-//let cm2 = new CodeMirror.fromTextArea(document.findElementById("entrada"));
-var cm3 = new CodeMirror.fromTextArea(document.getElementById("entrada").value, {lineNumbers: true});
 
 ejecutar.addEventListener('click',()=>{
     const entrada=document.getElementById("entrada").value
