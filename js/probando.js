1dc8a1ba4892499f35dd37780f3eb210228cd1ff
AbyPalencia <palenciaaby@gmail.com>
2021-12-12 13:54:13 -0600

    f grafo

--- js/probando.js
+++ js/probando.js
@@ -10,4 +10,25 @@ ejecutar.addEventListener('click',()=>{
     analizador.parse(entrada)
     consola.value=contenido
 })
+var redraw;
+window.height = 300;
+window.width = 400;
+var g = new Graph();
 
+g.addEdge("cherry", "apple");
+g.addEdge("strawberry", "kiwi");
+g.addEdge("banana", "banana");
+
+
+/* layout the graph using the Spring layout implementation */
+var layouter = new Graph.Layout.Spring(g);
+layouter.layout();
+
+/* draw the graph using the RaphaelJS draw implementation */
+var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
+renderer.draw();
+
+redraw = function() {
+  layouter.layout();
+  renderer.draw();
+};
