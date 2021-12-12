92c6c1a8149877a8daf6f90df6e66135e243280d
Gabriel <ludwing.paz@gmail.com>
2021-12-12 14:03:58 -0600

    comente lo de graph

--- js/probando.js
+++ js/probando.js
@@ -10,25 +10,25 @@ ejecutar.addEventListener('click',()=>{
     analizador.parse(entrada)
     consola.value=contenido
 })
-var redraw;
-window.height = 300;
-window.width = 400;
-var g = new Graph();
+// var redraw;
+// window.height = 300;
+// window.width = 400;
+// var g = new Graph();
 
-g.addEdge("cherry", "apple");
-g.addEdge("strawberry", "kiwi");
-g.addEdge("banana", "banana");
+// g.addEdge("cherry", "apple");
+// g.addEdge("strawberry", "kiwi");
+// g.addEdge("banana", "banana");
 
 
-/* layout the graph using the Spring layout implementation */
-var layouter = new Graph.Layout.Spring(g);
-layouter.layout();
+// /* layout the graph using the Spring layout implementation */
+// var layouter = new Graph.Layout.Spring(g);
+// layouter.layout();
 
-/* draw the graph using the RaphaelJS draw implementation */
-var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
-renderer.draw();
+// /* draw the graph using the RaphaelJS draw implementation */
+// var renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
+// renderer.draw();
 
-redraw = function() {
-  layouter.layout();
-  renderer.draw();
-};
+// redraw = function() {
+//   layouter.layout();
+//   renderer.draw();
+// };
